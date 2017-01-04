456253886cc329c2ba04cd376523c4a59b53aaf2
Rafael Martinez <raf_neta@hotmail.com>
2017-01-04 01:04:52 -0600

    avances

---
+++ js/ejeFou.js
@@ -0,0 +1,37 @@
+function Fou1o() {
+    document.getElementById("fou1").style.width = "100%";
+}
+
+function Fou1c() {
+    document.getElementById("fou1").style.width = "0%";
+}
+
+function Fou2o() {
+    document.getElementById("fou2").style.width = "100%";
+}
+
+function Fou2c() {
+    document.getElementById("fou2").style.width = "0%";
+}
+function Fou3o() {
+    document.getElementById("fou3").style.width = "100%";
+}
+
+function Fou3c() {
+    document.getElementById("fou3").style.width = "0%";
+}
+function Fou4o() {
+    document.getElementById("fou4").style.width = "100%";
+}
+
+function Fou4c() {
+    document.getElementById("fou4").style.width = "0%";
+}
+
+function Fou5o() {
+    document.getElementById("fou5").style.width = "100%";
+}
+
+function Fou5c() {
+    document.getElementById("fou5").style.width = "0%";
+}
